c13bd4c0307e0af0a109cbcb0c66f3e1acefc808
Alexander  Walker <awalkerbarreda@gmail.com>
2023-10-05 01:59:28 -0500

    cambios js

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ const listaNombres = [];
 const listaElement = document.getElementById("listaNombres");
 
 function agregarNombre() {
+    // Obtener el valor del campo de entrada de texto
     const nombreInput = document.getElementById("nombre");
     const nombre = nombreInput.value.trim();
 
+    // Verificar que el nombre no esté vacío
     if (nombre !== "") {
+        // Agregar el nombre a la matriz listaNombres
         listaNombres.push(nombre);
-        nombreInput.value = "";
+        nombreInput.value = ""; // Limpiar el campo de entrada
 
-        // Agregar el nombre a la lista
+        // Crear un nuevo elemento de lista (<li>) y agregarlo a la lista
         const li = document.createElement("li");
         li.innerHTML = `${nombre} <span class="borrar" onclick="borrarNombre(${listaNombres.length - 1})">Eliminar</span>`;
         listaElement.appendChild(li);
@@ -17,25 +20,14 @@ function agregarNombre() {
 }
 
 function borrarNombre(index) {
-    listaNombres.splice(index, 1);
-    const listItem = listaElement.childNodes[index];
-    listaElement.removeChild(listItem);
+    listaNombres.splice(index, 1); // Elimina el nombre de la matriz
+    const listItem = listaElement.childNodes[index]; // Encuentra el elemento de lista a eliminar
+    listaElement.removeChild(listItem); // Elimina el elemento de lista
 }
+
 
 function borrarNombres() {
-    listaNombres.length = 0;
-    listaElement.innerHTML = "";
+    listaNombres.length = 0; // Vacía la matriz listaNombres
+    listaElement.innerHTML = ""; // Elimina todos los elementos de la lista
 }
 
-function seleccionarAleatorio() {
-    if (listaNombres.length > 0) {
-        const randomIndex = Math.floor(Math.random() * listaNombres.length);
-        const selectedName = listaNombres[randomIndex];
-        
-        // Encuentra el div para mostrar el nombre seleccionado
-        const nombreSeleccionadoDiv = document.getElementById("nombreSeleccionado");
-        
-        // Muestra el nombre seleccionado en el div
-        nombreSeleccionadoDiv.textContent = `Nombre seleccionado: ${selectedName}`;
-    }
-}
